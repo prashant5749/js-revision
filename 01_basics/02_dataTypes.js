fe4273fc3bc1de3f863af9168bcbc0edbce675fe
prashant5749 <42830112+prashant5749@users.noreply.github.com>
2024-02-13 12:15:28 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS in newer version 
+
+let name = "Baaadal" //String datatype
+let age = 23 //number datatype
+let isLoggedIn = true //Boolean datatype
+
+//null => standalone value
+//undefined , when variable is declared but no value given to that variable
+//Symbol => use for Uniqueness
+
+//object
+
+console.log(typeof undefined) // outcome is undefined
+console.log(typeof null) //outcome is object
