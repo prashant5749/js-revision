9e5c241614e7ddfc2bcaf14da4661dcba56c54a6
prashant5749 <42830112+prashant5749@users.noreply.github.com>
2024-02-14 07:33:55 +0000

    datatypes

---
+++ 01_basics/04_dataTypesSummary.js
@@ -0,0 +1,27 @@
+// Primitive datatypes, they are seven types
+//1- String, 2- Number, 3- Boolean, 4- Null, 5- Undefined, 6- BigInt, 7- Symbol
+
+//Non-Primitive datatypes(reference), they are of mainly 3 types
+//1- Obejects, 2- Array, 3- Function
+
+let uniqueNumber = Symbol(33)
+let changeInSymbol = Symbol(33)
+
+// console.log(typeof changeInSymbol);
+
+let heros = ["spiderman", "batman", "ironman"]
+
+let myObjects = {
+    userName: "prashant",
+    userMobile: 80006449393,
+    userCity: "nagina"
+
+
+}
+
+console.table(myObjects);
+
+let myFun = function () {
+    // console.log("myFun");
+}
+
