d4a44dcadbc623b8ba8e9ec3661b14fb46f5beba
prashant5749 <42830112+prashant5749@users.noreply.github.com>
2024-02-14 05:46:11 +0000

    conversion of dataypes

---
+++ 01_basics/03_operationConversion.js
@@ -0,0 +1,23 @@
+let age = "24shsh"
+
+//console.log(typeof age)
+
+let convertNum = Number(age)
+//console.log(convertNum)
+
+// "24" => String
+// 24 => Number
+// "24kff" => NaN
+// null => 0
+// true => 1; false => 0
+// undefined => NaN
+
+let userLoggedIn = "prashant"
+
+let converUser = Boolean(userLoggedIn)
+console.log(converUser)
+
+//true => true
+//1 => true
+// "" => false
+// "prashant" => true
---
+++ 01_basics/practice/script.js
@@ -0,0 +1,16 @@
+const userId = "2546"
+let userEmail = "badal@gmail.com"
+let userPassword = 5555
+let userCity = "aligarh"
+let userInterest = "coding"
+let userHome;
+
+// userId = "akashmarale@gmail.com"
+// console.table([userEmail ,userPassword ,userCity ,userInterest])
+console.log(typeof userHome)
+// String
+// Number
+// Boolean
+// undefined
+// null
+// BigInt
