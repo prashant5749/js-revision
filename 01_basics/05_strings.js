bf9ab398117195f8bc8a6df6f32d45c7703de546
prashant5749 <42830112+prashant5749@users.noreply.github.com>
2024-02-14 12:27:06 +0000

    more about strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,22 @@
+const name = "Prashant"
+const age = 30
+
+// console.log(`My name is ${name} and my age is ${age}`);
+
+const namechng = new String("Baadal")
+// console.log(namechng.__proto__);
+// console.log(namechng[3]);
+
+// console.log(namechng.toLowerCase());
+// console.log(namechng.substring(0, 4));
+// console.log(namechng.slice(-6, 2));
+// console.log(namechng.indexOf("d"));
+// console.log(namechng.charAt(4));
+
+const lodaLehsan = "           bakchoodi-annu-baadal-loda           "
+// console.log(lodaLehsan);
+// console.log(lodaLehsan.trim());
+const url = "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment"
+// console.log(url.replace("/", "_"));
+// console.log(url.includes("prashant"));
+console.log(lodaLehsan.split("-"));
