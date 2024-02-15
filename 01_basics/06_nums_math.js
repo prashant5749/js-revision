9e774d5c61531802eda39ac0efe6db33f89028a1
prashant5749 <42830112+prashant5749@users.noreply.github.com>
2024-02-15 12:19:38 +0000

    nums and math

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,23 @@
+const number = 200
+
+// console.log(number.toFixed('2'));
+// console.log(number.toString().length);
+
+const anotherNumber = 1000000
+// console.log(anotherNumber.toLocaleString("en-IN"))
+
+const newNum = 123.43666
+// console.log(newNum.toPrecision(4));
+
+// **************************Maths*****************************
+// console.log(Math);
+
+// console.log(Math.abs(-6));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+// console.log(Math.round(4.5));
+// console.log(Math.random());
+
+const min = 1.00
+const max = 10000
+console.log(Math.floor(Math.random() * (max - min + 1)) + min );
