76de07902588ed155d57672a68675e65a668e908
prashant5749 <42830112+prashant5749@users.noreply.github.com>
2024-02-16 06:48:40 +0000

    about array methods

---
+++ 02_basics/01_array.js
@@ -0,0 +1,22 @@
+// Array
+
+const newArr = ["annu", "badal", "chohti", "munni"]
+
+// Second method to declare an array 
+
+const array = new Array(1, 2, 3, 4)
+
+// newArr.push("Prashant")
+// newArr.pop()  //It remove the last value from array
+
+// newArr.unshift("Prashant")  //It adds the new value to the starting of the array
+// newArr.shift() // It removes the first value from the array
+
+// const myArr = newArr.join()
+
+// console.log(newArr.join ());
+// console.log(newArr);
+
+// const myn1 = array.slice(1, 3) // It does not remove the value from the array
+// const myn1 = array.splice(1, 3) // It remove the value from the original database of array
+console.log("A        ", array);
