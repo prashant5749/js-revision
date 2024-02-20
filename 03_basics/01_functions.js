e6e44d450f4064329659c6b1d51ea8698b3c3141
prashant5749 <42830112+prashant5749@users.noreply.github.com>
2024-02-20 11:42:33 +0000

    passing object and arrays into functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -33,4 +33,32 @@ function userLoggedIn(username = "computer"){
 }
 
 // console.log(userLoggedIn("baadal k"))
-console.log(userLoggedIn("prashant"))
+// console.log(userLoggedIn("prashant"))
+
+function shoppingCart(val2,...val1){
+    return val1
+}
+
+// console.log(shoppingCart(100,200,500));
+
+//*********************How to pass object into functions*************************
+const person = {
+    wakup: 7,
+    sleep: "four_hours",
+    meditate: "daily"
+}
+
+function passingobject(anyObject){
+    console.log(`This person wakeup at ${anyObject.wakup} and sleep for ${anyObject.sleep}`);
+}
+
+// passingobject(person)
+
+const recentArray = [313, 2232, 4342, 123]
+
+function passingArray(getMyArray){
+    return getMyArray[1]
+}
+
+console.log(passingArray(recentArray));
+
