6724d35e4779e9096b1bb57f10d83180914c01b3
prashant5749 <42830112+prashant5749@users.noreply.github.com>
2024-02-23 06:12:48 +0000

    nullish coaelscing operators

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -13,5 +13,24 @@ const userEmail = []
 const emptyObj = {}
 
 if (Object.keys(emptyObj).length === 0) {
-    console.log("Object is empty");
+    // console.log("Object is empty");
 }
+
+// Nullish coalescing operator (??) : null, undefined
+
+// let val1;
+// val1 = 5 ?? 10
+// console.log(val1);
+
+
+let val1;
+// val1 = null ?? 10
+// val1 = undefined ?? 20
+val1 = null ?? 30 ?? 20
+// console.log(val1);
+
+// Terniary operator 
+// condition ? true : false
+
+const myTeaPrice = 200
+myTeaPrice <= 100 ? console.log("less than 100") : console.log("more than 100");
