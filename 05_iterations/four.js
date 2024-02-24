08bbb4b8acd3e951621017487b2322b014a349b1
prashant5749 <42830112+prashant5749@users.noreply.github.com>
2024-02-24 07:20:04 +0000

    loops

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -7,5 +7,12 @@ const newObject = {
 
 for (const key in newObject) {
     
-        console.log(key);
+        //console.log(`${key} name is : ${newObject[key]}`);
 }
+
+const members = ["annu", "munni", "chohti", "divya", "meethi"]
+for (const key in members) {
+    console.log(members[key]);
+}
+
+//We can not use for in loop on maps as Map is not iterable in this loop
