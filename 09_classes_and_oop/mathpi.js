b45d569eaf9d36d9c984649575c2102445443c24
prashant5749 <42830112+prashant5749@users.noreply.github.com>
2024-03-02 02:09:27 +0000

    about descripter and define property

---
+++ 09_classes_and_oop/mathpi.js
@@ -0,0 +1,34 @@
+const descripter = Object.getOwnPropertyDescriptor(Math, 'PI')
+// console.log(descripter);
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);
+
+const chai = {
+    name: 'Adrak vaali chai',
+    price: 20,
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log('Chai not prepared');
+    }
+}
+
+ const prop = Object.getOwnPropertyDescriptor(chai, 'price', 'name', 'isAvailable')
+
+Object.defineProperty(chai, 'name', {
+    // writable: false,
+    enumerable: true
+})
+// const prop = Object.getOwnPropertyDescriptor(chai, 'name')
+// chai.name = 'cold tea'
+// console.log(prop);
+// console.log(chai.name);
+
+for (let [key, value] of Object.entries(chai)) {
+    if(typeof value !== 'function'){
+        
+        console.log(`${key} : ${value}`);
+    }
+}
